e501b36593a7c40fce545edb94fe36caf66314a3
mohan-pannirselvam <mohan.pannirselvam08@gmail.com>
2019-09-08 01:10:07 +0200

    own coding

--- Webcamdebug1.py
+++ Webcamdebug1.py
@@ -1,8 +1,8 @@
 import numpy as np
 import cv2
- cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(0)
 
- while(True):
+while(True):
      #Capture the damn VideoCapture
      ret, frame = cap.read()
 
@@ -10,9 +10,10 @@ import cv2
      gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
      #Dispaly the result where
-     cv2.waitKey(1) & 0xFF == ord ('q');
-     break
+     cv2.imshow('frame',gray)
+     if cv2.waitKey(1) & 0xFF == ord ('q'):
+         break
 
 #when everythinbg is done
 cap.release()
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
